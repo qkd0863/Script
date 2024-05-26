fa71a25ddcbd84d6d87b0948910e617a78c8bbae
qkd0863 <58385389+qkd0863@users.noreply.github.com>
2024-05-26 21:36:43 +0900

    시 리스트박스

--- xml/main.py
+++ xml/main.py
@@ -1,41 +1,81 @@
 import requests
 import xml.etree.ElementTree as ET
-import tkinter
+from tkinter import *
+from tkinter import font
 
 # 병원정보 서비스 예제
 
-url = 'https://openapi.gg.go.kr/TBGGIBLLBR'
-service_key = "94c80014751c43c3aff04a8290cda503"
-queryParams = {'KEY': service_key, 'Type': 'xml', 'pIndex': '1', 'pSize': '100'}
+window = Tk()
+window.geometry("1200x800+200+200")
 
-response = requests.get(url, params=queryParams)
-print(response.text)
-root = ET.fromstring(response.text)
 
-window = tkinter.Tk()
-window.title("도서관 정보")
+class MainGui():
+    url = 'https://openapi.gg.go.kr/TBGGIBLLBR'
+    service_key = "94c80014751c43c3aff04a8290cda503"
+    queryParams = {'KEY': service_key, 'Type': 'xml', 'pIndex': '1', 'pSize': '100'}
+    response = requests.get(url, params=queryParams)
+    root = ET.fromstring(response.text)
+    List = set()
 
-frame = tkinter.Frame(window)
-frame.pack()
+    def __init__(self):
 
-header = ["Name", "Addr", "Tel", "Url"]
+        window.title("도서관 정보")
 
-for i, col_name in enumerate(header):
-    label = tkinter.Label(frame, text=col_name, font=("Helvetica", 14, "bold"))
-    label.grid(row=0, column=i)
+        frame = Frame(width=100,height=80)
+        frame.place(x=200,y=200)
 
-row_count = 1
-for item in root.iter("row"):
-    SIGUN_NM = item.findtext("SIGUN_NM")
-    LOCPLC_ADDR = item.findtext("LOCPLC_ADDR")
-    TELNO = item.findtext("TELNO")
-    HMPG_ADDR = item.findtext("HMPG_ADDR")
+        header = ["Name", "Addr", "Tel", "Url"]
 
-    data = [SIGUN_NM, LOCPLC_ADDR, TELNO, HMPG_ADDR]
-    for i, value in enumerate(data):
-        label = tkinter.Label(frame, text=value, font=("Helvetica", 12))
-        label.grid(row=row_count, column=i)
+        for i, col_name in enumerate(header):
+            label = Label(frame, text=col_name, font=("Helvetica", 14, "bold"))
+            label.grid(row=0, column=i)
 
-    row_count += 1
+        row_count = 1
+        for item in self.root.iter("row"):
+            SIGUN_NM = item.findtext("SIGUN_NM")
+            self.List.add(item.findtext("SIGUN_NM"))
+            LOCPLC_ADDR = item.findtext("LOCPLC_ADDR")
+            TELNO = item.findtext("TELNO")
+            HMPG_ADDR = item.findtext("HMPG_ADDR")
 
-window.mainloop()
+            #data = [SIGUN_NM, LOCPLC_ADDR, TELNO, HMPG_ADDR]
+            #for i, value in enumerate(data):
+            #    label = Label(frame, text=value, font=("Helvetica", 12))
+            #    label.grid(row=row_count, column=i)
+
+            row_count += 1
+
+        self.InitTopText()
+        self.InitSearchListBox()
+
+        window.mainloop()
+
+    def InitTopText(self):
+        self.TopText = Label(window, text="경기도 도서관 정보 검색")
+        self.TopText.pack()
+        self.TopText.place(x=20, y=0)
+
+    def InitSearchListBox(self):
+        SearchFrame=Frame(width=100,height=100)
+        SearchFrame.place(x=10,y=10)
+
+        global SearchListBox
+        ListboxScrollbar = Scrollbar(SearchFrame)
+        ListboxScrollbar.pack(side='right',fill='y')
+        ListboxScrollbar.place(x=150, y=200)
+
+        TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+        SearchListBox = Listbox(SearchFrame, font=TempFont, activestyle='none', width=10, height=5, borderwidth=12,
+                                relief='ridge', yscrollcommand=ListboxScrollbar.set)
+
+        num = 0
+        for i in self.List:
+            SearchListBox.insert(num + 1, i)
+            num += 1
+
+
+        SearchListBox.place(x=10, y=200)
+        ListboxScrollbar.config(command=SearchListBox.yview)
+
+
+MainGui()
--- xml/인기대출.py
+++ xml/인기대출.py
@@ -1,44 +1,45 @@
 import requests
 import xml.etree.ElementTree as ET
-import tkinter
+from tkinter import *
 
-# 병원정보 서비스 예제
+window = Tk()
 
 
-url = 'http://data4library.kr/api/loanItemSrch'
+class MainGui:
+    url = 'http://data4library.kr/api/loanItemSrch'
+    service_key = "f1e7e93ee1351c8e15709881906543fb843305921a8d66a87f30050077959fcb"
+    queryParams = {'authKey': service_key, 'startDt': '2022-01-01', 'endDt': '2024-05-01'}
 
-service_key = "f1e7e93ee1351c8e15709881906543fb843305921a8d66a87f30050077959fcb"
+    def __init__(self):
+        self.response = requests.get(self.url, params=self.queryParams)
+        self.root = ET.fromstring(self.response.text)
 
-queryParams = {'authKey': service_key, 'startDt': '2022-01-01', 'endDt': '2024-05-01'}
+        window.title("도서관 정보")
+        frame = Frame(window)
+        frame.pack()
 
-response = requests.get(url, params=queryParams)
-print(response.text)
-root = ET.fromstring(response.text)
+        header = ["Name", "Addr", "Tel", "Url"]
 
-window = tkinter.Tk()
-window.title("도서관 정보")
+        for i, col_name in enumerate(header):
+            label = Label(frame, text=col_name, font=("Helvetica", 14, "bold"))
+            label.grid(row=0, column=i)
 
-frame = tkinter.Frame(window)
-frame.pack()
+        row_count = 1
+        for item in self.root.iter("row"):
+            SIGUN_NM = item.findtext("SIGUN_NM")
+            LOCPLC_ADDR = item.findtext("LOCPLC_ADDR")
+            TELNO = item.findtext("TELNO")
+            HMPG_ADDR = item.findtext("HMPG_ADDR")
 
-header = ["Name", "Addr", "Tel", "Url"]
+            data = [SIGUN_NM, LOCPLC_ADDR, TELNO, HMPG_ADDR]
+            for i, value in enumerate(data):
+                label = Label(frame, text=value, font=("Helvetica", 12))
+                label.grid(row=row_count, column=i)
 
-for i, col_name in enumerate(header):
-    label = tkinter.Label(frame, text=col_name, font=("Helvetica", 14, "bold"))
-    label.grid(row=0, column=i)
+            row_count += 1
 
-row_count = 1
-for item in root.iter("row"):
-    SIGUN_NM = item.findtext("SIGUN_NM")
-    LOCPLC_ADDR = item.findtext("LOCPLC_ADDR")
-    TELNO = item.findtext("TELNO")
-    HMPG_ADDR = item.findtext("HMPG_ADDR")
+        window.mainloop()
 
-    data = [SIGUN_NM, LOCPLC_ADDR, TELNO, HMPG_ADDR]
-    for i, value in enumerate(data):
-        label = tkinter.Label(frame, text=value, font=("Helvetica", 12))
-        label.grid(row=row_count, column=i)
 
-    row_count += 1
 
-window.mainloop()
+MainGui()
